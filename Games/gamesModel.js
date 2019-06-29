3e5904254fe80f5e3533859060ad1586d04a296c
Rane <rane@blasph.me>
2019-06-28 18:38:17 -0700

    adding db configuration files and stubbing in db helper

---
+++ Games/gamesModel.js
@@ -0,0 +1,29 @@
+const db = require('../data/config');
+
+const getAll = () => {
+    return null;
+}
+
+const getById = id => {
+    return null;
+}
+
+const insertGame = game => {
+    return null;
+}
+
+const updateGame = id => {
+    return null;
+}
+
+const deleteGame = id => {
+    return null;
+}
+
+module.exports = {
+    getAll,
+    getById,
+    insertGame,
+    updateGame,
+    deleteGame
+}
